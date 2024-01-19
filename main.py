2ec9c8f6138db36d259c0108371e4cb26e0a49c2
saidali-tsaravelonaallaoui <said-ali.tsaravelona-allaoui@laplateforme.io>
2024-01-19 11:12:50 +0100

    mise a jour du menu, modif position des bouttons, ajout nvlle couleur

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import pygame, sys
 from Game.Class.bouton import Bouton
 
-
 pygame.init()
-
 SCREEN = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Menu")
 
@@ -12,18 +10,18 @@ BG = pygame.image.load("../pokemon/Assets/menu/bg.png")
 def get_font(size): 
     return pygame.font.Font("../pokemon/Assets/menu/font.ttf", size)
 
-def play():
+def JOUER():
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
-        SCREEN.fill("black")
+        SCREEN.fill("white")
 
-        PLAY_TEXT = get_font(45).render("CHOIX DU POKEMON", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        PLAY_TEXT = get_font(45).render("CHOIX DU POKEMON", True, "Black")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 70))
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
 
-        PLAY_RETOUR = Bouton(image=None, pos=(640, 460), 
-                            text_input="RETOUR", font=get_font(75), base_color="White", hovering_color="Green")
+        PLAY_RETOUR = Bouton(image=None, pos=(100, 650), 
+                            text_input="RETOUR", font=get_font(40), base_color="Black", hovering_color="#c047c0")
 
         PLAY_RETOUR.changeColor(PLAY_MOUSE_POS)
         PLAY_RETOUR.update(SCREEN)
@@ -45,11 +43,11 @@ def REPRENDRE():
         SCREEN.fill("white")
 
         REPRENDRE_TEXT = get_font(45).render("POKEDEX!!!!!!", True, "Black")
-        REPRENDRE_RECT = REPRENDRE_TEXT.get_rect(center=(640, 260))
+        REPRENDRE_RECT = REPRENDRE_TEXT.get_rect(center=(640, 70))
         SCREEN.blit(REPRENDRE_TEXT, REPRENDRE_RECT)
 
-        REPRENDRE_RETOUR = Bouton(image=None, pos=(640, 460), 
-                            text_input="RETOUR", font=get_font(75), base_color="Black", hovering_color="Green")
+        REPRENDRE_RETOUR = Bouton(image=None, pos=(100, 650), 
+                            text_input="RETOUR", font=get_font(40), base_color="Black", hovering_color="#c047c0")
 
         REPRENDRE_RETOUR.changeColor(REPRENDRE_MOUSE_POS)
         REPRENDRE_RETOUR.update(SCREEN)
@@ -61,7 +59,7 @@ def REPRENDRE():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if REPRENDRE_RETOUR.checkForInput(REPRENDRE_MOUSE_POS):
                     main_menu()
-
+                    
         pygame.display.update()
 
 def main_menu():
@@ -70,17 +68,12 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(100).render("", True, "#f9cc36")
-        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
-
         PLAY_Bouton = Bouton(image=pygame.image.load("../pokemon/Assets/menu/Play_Rect.png"), pos=(640, 250), 
                             text_input="NOUVELLE PARTIE", font=get_font(60), base_color="White", hovering_color="#c047c0")
         REPRENDRE_Bouton = Bouton(image=pygame.image.load("../pokemon/Assets/menu/Reprendre_Rect.png"), pos=(640, 400), 
                             text_input="REPRENDRE", font=get_font(60), base_color="White", hovering_color="#c047c0")
         QUIT_Bouton = Bouton(image=pygame.image.load("../pokemon/Assets/menu/Quitter_Rect.png"), pos=(640, 550), 
                             text_input="QUITTER", font=get_font(60), base_color="White", hovering_color="#c047c0")
-
-        SCREEN.blit(MENU_TEXT, MENU_RECT)
 
         for bouton in [PLAY_Bouton, REPRENDRE_Bouton, QUIT_Bouton]:
             bouton.changeColor(MENU_MOUSE_POS)
@@ -92,7 +85,7 @@ def main_menu():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_Bouton.checkForInput(MENU_MOUSE_POS):
-                    play()
+                    JOUER()
                 if REPRENDRE_Bouton.checkForInput(MENU_MOUSE_POS):
                     REPRENDRE()
                 if QUIT_Bouton.checkForInput(MENU_MOUSE_POS):
@@ -100,5 +93,5 @@ def main_menu():
                     sys.exit()
 
         pygame.display.update()
-
+        
 main_menu()
