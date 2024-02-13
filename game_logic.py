828b450ca043c9a27a3f441138dc1a37b8ca1b6a
saidali-tsaravelonaallaoui <said-ali.tsaravelona-allaoui@laplateforme.io>
2024-02-13 14:37:27 +0100

    ajout d'une classe pokemon

--- game_logic.py
+++ game_logic.py
@@ -41,3 +41,45 @@ class Move():
         self.name = self.json['name']
         self.power = self.json['power']
         self.type = self.json['type']
+        
+# Classe Pokemon
+class Pokemon(pygame.sprite.Sprite):
+    
+    # définir la méthode d'initialisation de la classe
+    def __init__(self, name, niveau, x, y):
+        with open(pokemon_json, "r") as file:
+            data = json.load(file)
+        
+        # Accédez au premier élément de la liste "Niveau1"
+        pokemon_data = data["Niveau1"][0]
+
+        # Trouvez le Pokémon spécifique par son nom dans la liste
+        for pokemon in data["Niveau1"]:
+            if pokemon["nom"] == name:
+                pokemon_data = pokemon
+                break
+
+        self.json = pokemon_data
+        self.name = name
+        self.niveau = niveau
+        self.x = x
+        self.y = y
+        self.num_potions = 3
+        
+        self.type = self.json['type']
+        self.PV = self.json['PV'] + self.niveau
+        self.MAX_PV = self.json['PV'] + self.niveau
+        self.Attaque = self.json['Attaque']
+        self.AttaqueSp = self.json['AttaqueSp']
+        self.Defense = self.json['Defense']
+        self.Vitesse = self.json['Vitesse']
+        
+        self.types = []
+        for i in range(len(self.type)):
+            type = self.json['type'][i]
+            self.types.append(type)
+            
+        self.size = 150
+            
+        # 
+        self.set_sprite('droite')
